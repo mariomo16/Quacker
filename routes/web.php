b7feb03fde493c9f324a30bf43c7d3f10f837493
mariomo16 <mariomo16.cieza@gmail.com>
2025-11-18 17:19:43 +0100

    Añadir modelo Quack, factory y migración para la tabla quacks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Quack;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/quacks', function () {
+    return Quack::get(); // Esto devolverá un JSON con todos los quacks
 });
