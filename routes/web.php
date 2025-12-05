636632504e8aa68565de34b4cca0bb3b18ad92f3
mariomo16 <mariomo16.cieza@gmail.com>
2025-12-05 17:39:08 +0100

    quashtag route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\QuackController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\QuashtagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('quacks', QuackController::class);
 Route::resource('users', UserController::class);
+Route::resource('quashtags', QuashtagController::class);
