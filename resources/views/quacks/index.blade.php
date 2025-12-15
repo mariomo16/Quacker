d031a7174a581cdae8f4c0840f3787bd756986c6
Mario Ortega <mariomo16.cieza@gmail.com>
2025-12-15 22:05:50 +0100

    Añadir validación y feedback de errores en formularios (#12)
    - Validar datos al crear y editar objetos
    - Mostrar errores de validación en la interfaz

--- resources/views/quacks/index.blade.php
+++ resources/views/quacks/index.blade.php
@@ -4,96 +4,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quacks / Quacker</title>
-    <style>
-        main {
-            width: 80%;
-            margin: 0 auto;
-        }
-
-        article {
-            background-color: lightcyan;
-            padding: 10px;
-            margin: 20px 0;
-            border-radius: 10px;
-            transition: all 0.3s ease;
-            box-shadow: 5px 5px 5px rgb(0, 0, 0, 0.5);
-        }
-
-        article:hover {
-            transform: scale(1.05);
-            box-shadow: 10px 10px 10px rgb(0, 0, 0, 0.5);
-        }
-
-        button {
-            border-radius: 10px;
-            padding: 5px 10px;
-            border: none;
-            background-color: lightblue;
-        }
-
-        div.btn {
-            position: fixed;
-            left: 20px;
-            transition: all 0.2 ease;
-        }
-        
-        div.btn:hover {
-            transform: scale(1.1);
-        }
-        
-        div.btn p {
-            font-size: 2rem;
-            background-color: lightblue;
-            margin: 0;
-            padding: 10px;
-            border-radius: 50%;
-            cursor: pointer;
-        }
-
-        div.btn a {
-            text-decoration: none;
-        }
-
-        div.quackea {
-            top: 20px;
-        }
-
-        div.users {
-            top: 100px;
-        }
-
-        div.quashtags {
-            top: 180px;
-        }
-    </style>
+    <title>Quacks / {{ config('app.name') }}</title>
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
     <main>
         @foreach ($quacks as $quack)
             <article>
-                <h3>{{ $quack->display_name }} ({{ $quack->created_at }})</h3>
+                <p><b>{{ $quack->display_name }}</b> <span
+                        class="text-muted">{{ '@' }}{{ $quack->display_name }}
+                        ·
+                        {{ $quack->created_at->diffForHumans(null, true, true, 1) }}</span></p>
                 <p>{{ $quack->content }}</p>
-                <p><a href="/quacks/{{ $quack->id }}">Ver más detalles</a></p>
-                <form action="/quacks/{{ $quack->id }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button>Eliminar</button>
-                </form>
-                <p><a href="/quacks/{{ $quack->id }}/edit">Editar</a></p>
+                <div class="resource-actions">
+                    <a href="/quacks/{{ $quack->id }}">Mostrar más</a>
+                    <a href="/quacks/{{ $quack->id }}/edit">Editar</a>
+                    <form method="POST" action="/quacks/{{ $quack->id }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn-delete">Eliminar</button>
+                    </form>
+                </div>
             </article>
         @endforeach
     </main>
-    <div class="btn quackea">
-        <p><a href="/quacks/create">➕</a></p>
-    </div>
-    <div class="btn users">
-        <p><a href="/users">👤</a></p>
-    </div>
-    <div class="btn quashtags">
-        <p><a href="/quashtags">#️⃣</a></p>
-    </div>
+    <nav class="nav-menu">
+        <a href="/quacks/create"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+                fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+            </svg></a>
+        <a href="/users"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                class="bi bi-person" viewBox="0 0 16 16">
+                <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+            </svg></a>
+        <a href="/quashtags"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                class="bi bi-hash" viewBox="0 0 16 16">
+                <path
+                    d="M8.39 12.648a1 1 0 0 0-.015.18c0 .305.21.508.5.508.266 0 .492-.172.555-.477l.554-2.703h1.204c.421 0 .617-.234.617-.547 0-.312-.188-.53-.617-.53h-.985l.516-2.524h1.265c.43 0 .618-.227.618-.547 0-.313-.188-.524-.618-.524h-1.046l.476-2.304a1 1 0 0 0 .016-.164.51.51 0 0 0-.516-.516.54.54 0 0 0-.539.43l-.523 2.554H7.617l.477-2.304c.008-.04.015-.118.015-.164a.51.51 0 0 0-.523-.516.54.54 0 0 0-.531.43L6.53 5.484H5.414c-.43 0-.617.22-.617.532s.187.539.617.539h.906l-.515 2.523H4.609c-.421 0-.609.219-.609.531s.188.547.61.547h.976l-.516 2.492c-.008.04-.015.125-.015.18 0 .305.21.508.5.508.265 0 .492-.172.554-.477l.555-2.703h2.242zm-1-6.109h2.266l-.515 2.563H6.859l.532-2.563z" />
+            </svg></a>
+    </nav>
 </body>
 
 </html>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,38 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tu cuenta / Quacker</title>
-    <style>
-        main {
-            width: 80%;
-            margin: 0 auto;
-        }
-
-        button {
-            border-radius: 10px;
-            padding: 5px 10px;
-            border: none;
-            background-color: lightblue;
-        }
-    </style>
+    <title>{{ '@' }}{{ $user->username }} / {{ config('app.name') }}</title>
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
     <main>
-        <form action="/users/{{ $user->id }}" method="POST">
-            <label>
-                Nombre: <input type="text" name="display_name" placeholder="Nombre" value="{{ $user->display_name }}">
-            </label><br>
-            <label>
-                Nombre de usuario: <input type="text" name="username" placeholder="Nombre"
-                    value="{{ $user->username }}">
-            </label><br>
-            <label>
-                Correo electrónico: <input type="text" name="email" placeholder="Nombre" value="{{ $user->email }}">
-            </label><br>
-            <button>Guardar</button>
+        <form method="POST" action="/users/{{ $user->id }}" class="resource-form">
             @csrf
             @method('PATCH')
+            <label>
+                <span class="text-muted">Nombre</span>
+                @error('display_name')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+                <input type="text" name="display_name" placeholder="Usuario Quacker" value="{{ $user->display_name }}"
+                    required>
+            </label>
+            <label>
+                <span class="text-muted">Nombre de usuario</span>
+                @error('username')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+                <input type="text" name="username" placeholder="usuario_quacker" value="{{ $user->username }}"
+                    required>
+            </label>
+            <label>
+                <span class="text-muted">Correo electrónico</span>
+                @error('email')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+                <input type="text" name="email" placeholder="usuario@quacker.es" value="{{ $user->email }}"
+                    required>
+            </label>
+            <label>
+                <span class="text-muted">Correo electrónico</span>
+                @error('password')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+                <input type="password" name="password" placeholder="P@ssw0rd"" required>
+            </label>
+            <div class="resource-actions resource-actions--end">
+                <a href="/users" class="btn-cancel">Cancelar</a>
+                <button type="submit" class="btn-save">Guardar</button>
+            </div>
         </form>
     </main>
 </body>
