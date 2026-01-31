ce8a58cc0d71bde372ae18e0e18fbbb889c7e122
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-31 19:08:51 +0100

    Fix: si el usuario no tiene quacks, no aparecia un 0 en quavs/requacks de ese usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,11 +16,11 @@
                         </div>
                         <div class="user-quav">
                             <x-icon.quav />
-                            {{ $user->quavs_count }}
+                            {{ $user->quavs_count ?: '0' }}
                         </div>
                         <div class="user-requack">
                             <x-icon.requack />
-                            {{ $user->requacks_count }}
+                            {{ $user->requacks_count ?: '0' }}
                         </div>
                     </div>
                     <div class="user-name">
