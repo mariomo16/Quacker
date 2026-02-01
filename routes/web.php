dcaaa22559521644802b3a422e01f4077bd5ec5f
mariomo16 <mariomo16.cieza@gmail.com>
2026-02-01 18:54:31 +0100

    patos patos patos

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use App\Http\Controllers\QuackController;
 use App\Http\Controllers\QuashtagController;
-use App\Http\Controllers\QuashtagQuackController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\UserQuackController;
+use App\Http\Controllers\QuashtagQuackController;
 use Illuminate\Support\Facades\Route;
 
 // Rutas para invitados (no autenticados)
 Route::middleware('guest')->group(function () {
-    Route::redirect('/', 'login');
+    Route::get('/', fn() => redirect()->route('login'));
 
     Route::get('/register', [AuthController::class, 'create'])->name('register');
     Route::post('/register', [AuthController::class, 'store']);
@@ -23,9 +23,9 @@ Route::middleware('guest')->group(function () {
 
 // Rutas protegidas (requieren autenticación)
 Route::middleware('auth')->group(function () {
-    Route::redirect('/', 'feed');
-
+    Route::get('/', fn() => redirect()->route('feed'));
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
     Route::get('/feed', [FeedController::class, 'index'])->name('feed');
 
     Route::resource('quacks', QuackController::class)->except('create');
